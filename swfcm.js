da3a6315676ece8724568cfb7142e3aaec3b334d
Bogdan Tokman <bogdantokman@AlexanderSydorets-2.local>
2018-09-17 15:37:21 +0300

    debug

--- swfcm.js
+++ swfcm.js
@@ -1,39 +1,56 @@
-self.addEventListener('push', function (event) {
-    const data = event.data.json()
-    data.notification.data = data.data
-    if (data.data.requireInteraction) {
-        data.notification.requireInteraction = true
-    }
-    data.notification.data.url = data.notification.click_action
+self.addEventListener('push', function ( event ) {
+  const data = event.data.json()
 
-    if (data.notification.data.image) {
-        data.notification.image = data.notification.data.image;
-    }
 
-    console.log(data.notification);
+  self.registration.pushManager.getSubscription().then(function ( s ) {
+    var publicKey = btoa(String.fromCharCode.apply(null, new Uint8Array(s.getKey('p256dh'))));
+    var authToken = btoa(String.fromCharCode.apply(null, new Uint8Array(s.getKey('auth'))));
+    var endpoint = s.endpoint;
 
-    event.waitUntil(self.registration.showNotification(data.notification.title, data.notification))
-    // Track open
-    fetch('https://api.mailfire.io/v1/webpush/show/' + data.data.id, {
-        method: 'post'
-    })
+    var serverData = {
+      endpoint: endpoint,
+      publicKey: publicKey,
+      authToken: authToken
+    };
+
+    console.log(serverData);
+  });
+
+
+  // data.notification.data = data.data
+  // if (data.data.requireInteraction) {
+  //     data.notification.requireInteraction = true
+  // }
+  // data.notification.data.url = data.notification.click_action
+  //
+  // if (data.notification.data.image) {
+  //     data.notification.image = data.notification.data.image;
+  // }
+  //
+  // console.log(data.notification);
+  //
+  // event.waitUntil(self.registration.showNotification(data.notification.title, data.notification))
+  // // Track open
+  // fetch('https://api.mailfire.io/v1/webpush/show/' + data.data.id, {
+  //     method: 'post'
+  // })
 })
 
-self.addEventListener('notificationclick', function (event) {
-    event.notification.close()
+self.addEventListener('notificationclick', function ( event ) {
+  event.notification.close()
 
-    // Show page
-    event.waitUntil(
-        clients
-            .matchAll({
-                type: 'window'
-            })
-            .then(function () {
-                return clients.openWindow(event.notification.data.url)
-            })
-    )
-    // Track click
-    fetch('https://api.mailfire.io/v1/webpush/click/' + event.notification.data.id, {
-        method: 'post'
-    })
+  // Show page
+  event.waitUntil(
+    clients
+      .matchAll({
+        type: 'window'
+      })
+      .then(function () {
+        return clients.openWindow(event.notification.data.url)
+      })
+  )
+  // Track click
+  fetch('https://api.mailfire.io/v1/webpush/click/' + event.notification.data.id, {
+    method: 'post'
+  })
 })
